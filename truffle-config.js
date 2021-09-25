88a8dddac97f4b98204a873948c04a95933a7227
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-09-25 14:20:47 -0400

    configurations

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+require('babel-register')
+require('babel-polyfill')
+
+module.exports = {
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: '7545',
+            network_id: '*' //connect to all kind of network
+        }
+    },
+    contracts_directory: './src/contracts',
+    contracts_build_directory: './src/truffle_abis',
+    compilers: {
+        solc: {
+            version: '^0.5.0',
+            optimizer: true,
+            runs: 200
+        }
+    }
+}
